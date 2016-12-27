f0aed24d90b937acc399ca09a2db1928927542b0
Dusan Jeftic <dusan.jeftic11@gmail.com>
2016-12-27 12:12:40 +0100

    Dodavanje iymena koje prethodno nisu povučene (ClientDialogViewModel)

---
+++ Moduo1/Client/ViewModel/ClientDialogViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Client.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ViewModel
+{
+    class ClientDialogViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewProjectsCommand ViewProjectsCommand { get; set; }
+        public AddEmployeeCommand AddEmployeeCommand { get; set; }
+        public ChangePasswordCommand ChangePasswordCommand { get; set; }
+        public EditPersonalDataCommand EditPersonalDataCommand { get; set; }
+        public SendRequestCommand SendRequestCommand { get; set; }
+        public ViewWorkersCommand ViewWorkersCommand { get; set; }
+        public WorkingHoursCommand WorkingHoursCommand { get; set; }
+
+        /// <summary>
+        /// private constructor
+        /// </summary>
+        public ClientDialogViewModel()
+        {
+            this.ViewProjectsCommand = new ViewProjectsCommand();
+            this.AddEmployeeCommand = new AddEmployeeCommand();
+            this.ChangePasswordCommand = new ChangePasswordCommand();
+            this.EditPersonalDataCommand = new EditPersonalDataCommand();
+            this.SendRequestCommand = new SendRequestCommand();
+            this.ViewWorkersCommand = new ViewWorkersCommand();
+            this.WorkingHoursCommand = new WorkingHoursCommand();
+        }
+    }
+}
