03fbac99e88e883146e3a5caf8f5d81c786db970
mmilan93 <muticmilan93@gmail.com>
2016-12-28 11:14:55 +0100

    Komitovanje imzena koje nisu prošlezadnji put.

---
+++ Moduo1/Client/Command/SignInCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Client.Command
+{
+    public class SignInCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            Object[] parameters = parameter as Object[];
+            if(parameters == null || parameters.Length != 2 
+                || parameters[0].ToString().Trim().Equals("")
+                || parameters[1].ToString().Trim().Equals(""))
+            {
+                MessageBox.Show("Niste popunili sva polja!",
+                                "Upozorenje",
+                                 MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+
+        }
+    }
+}
---
+++ Moduo1/Client/Model/LogInUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    public class LogInUser
+    {
+        private string username;
+        private string password;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public LogInUser()
+        {
+            this.username = "";
+            this.password = "";
+        }
+
+        public LogInUser(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
