ac1cc6fd6bccbd7c274176c5d2eb4d4a5ca8d452
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-01-04 12:03:07 +0100

    Dodat settings style cop file koji je falio u test klasi.

---
+++ Moduo1/Client/Command/SendRequestProjectViewCommand.cs
@@ -0,0 +1,30 @@
+﻿using Client.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Client.Command
+{
+    /// <summary>
+    /// samo omogucuje prikaz guia u glavnom prozoru
+    /// </summary>
+    public class SendRequestProjectViewCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine("Izvrsavanje prikaya u toku");
+            ClientDialogViewModel.Instance.ShowSendRequestProjectView();
+        }
+    }
+}
