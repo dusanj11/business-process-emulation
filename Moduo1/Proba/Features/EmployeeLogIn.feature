56669d622138742eb6f37f4cdaf2d772ebc8bee0
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-01-11 02:36:23 +0100

    Dodat feature file i odgovarjuca steps klasa za menjanje statusa logovanosti zaposlenog.

---
+++ Moduo1/Proba/Features/EmployeeLogIn.feature
@@ -0,0 +1,16 @@
+﻿Feature: EmployeeLogIn
+	In order to mark my arrival at work
+	As an employee
+	I want to be able to change my status to logged in
+
+@DusanBDD
+Scenario: Entered correct username
+	When I have entered valid username
+	And I request to change my log status
+	Then the result should be returning true
+
+@DusanBDD
+Scenario: Entered incorrect username
+	When I have entered invalid username
+	And I have requested to update my log status
+	Then the result should not be returning true
---
+++ Moduo1/Proba/Steps/EmployeeLogInSteps.cs
@@ -0,0 +1,52 @@
+﻿using Client;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Proba.Steps
+{
+    [Binding]
+    public class EmployeeLogInSteps
+    {
+        private static string us1;
+        private static string us2;
+        private static bool val1;
+        private static bool val2;
+
+        [When(@"I have entered valid username")]
+        public void WhenIHaveEnteredValidUsername()
+        {
+            us1 = "dule";
+        }
+        
+        [When(@"I request to change my log status")]
+        public void WhenIRequestToChangeMyLogStatus()
+        {
+            val1 = ClientProxy.Instance.EmployeeLogIn(us1);
+        }
+        
+        [When(@"I have entered invalid username")]
+        public void WhenIHaveEnteredInvalidUsername()
+        {
+            us2 = "ufieuer";
+        }
+        
+        [When(@"I have requested to update my log status")]
+        public void WhenIHaveRequestedToUpdateMyLogStatus()
+        {
+            val2 = ClientProxy.Instance.EmployeeLogIn(us2);
+        }
+        
+        [Then(@"the result should be returning true")]
+        public void ThenTheResultShouldBeReturningTrue()
+        {
+            Assert.AreEqual(val1, true);
+        }
+        
+        [Then(@"the result should not be returning true")]
+        public void ThenTheResultShouldNotBeReturningTrue()
+        {
+            Assert.AreEqual(val2, false);
+        }
+    }
+}
