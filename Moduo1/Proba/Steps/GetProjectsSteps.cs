ff89b5c000411ff9b22842c525cf56fb651d17e6
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-01-11 03:39:52 +0100

    Dodati testni fajlovi za funkciju preuzimanja svih trenuntih projekata.

---
+++ Moduo1/Proba/Steps/GetProjectsSteps.cs
@@ -0,0 +1,39 @@
+﻿using Client;
+using HiringCompanyData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace Proba.Steps
+{
+    [Binding]
+    public class GetProjectsSteps
+    {
+        private static List<Project> lp = new List<Project>();
+
+        [When(@"I have requested to see current projects")]
+        public void WhenIHaveRequestedToSeeCurrentProjects()
+        {
+            lp = ClientProxy.Instance.GetProjects();
+        }
+        
+        [When(@"I have requested current projects data")]
+        public void WhenIHaveRequestedCurrentProjectsData()
+        {
+            lp = ClientProxy.Instance.GetProjects();
+        }
+        
+        [Then(@"the result should be a list of projects")]
+        public void ThenTheResultShouldBeAListOfProjects()
+        {
+            Assert.That(lp.Count > 0, Is.True);
+        }
+        
+        [Then(@"the result should be null or empty list")]
+        public void ThenTheResultShouldBeNullOrEmptyList()
+        {
+            Assert.That(lp.Count == 0, Is.True);
+        }
+    }
+}
