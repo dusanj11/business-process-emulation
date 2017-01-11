ab742b89a0301ac512d20170063442e61751ded9
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-01-11 16:09:32 +0100

    napravljena metoda koja salje mejl product ownerima, ciji projekat se nalazi u alarmantnom stanju.

--- Moduo1/HiringCompanyService/Program.cs
+++ Moduo1/HiringCompanyService/Program.cs
@@ -132,7 +132,8 @@ namespace HiringCompanyService
                 //    Thread checkThread = new Thread(new ThreadStart(del.CheckIfSomeoneIsLate));
                 //    checkThread.Start();
                 //}
-                    
+                //Thread alarmPo = new Thread(new ThreadStart(del.CheckIfProjectAlmostLate));
+                //alarmPo.Start();
 
                 Console.ReadLine();
 
@@ -154,12 +155,34 @@ namespace HiringCompanyService
         public void CheckIfProjectAlmostLate()
         {
             List<Project> currentProjects = new List<Project>(30);
-
+            Employee po = new Employee();
             while(currentProjects.Count != 0)
             {
                 foreach(Project proj in currentProjects)
                 {
+                    if ((proj.Progress <= 8.00) && ((proj.EndDate.Month == DateTime.Now.Month) && ((proj.EndDate.Day - DateTime.Now.Day) <= 10)))
+                    {
+                        po = proj.ProductOwner;
 
+                        using (SmtpClient smtpClient = new SmtpClient())
+                        {
+                            using (MailMessage message = new MailMessage())
+                            {
+                                message.Subject = "ALARMNO STANJE PROJEKAT NIJE GOTOV!";
+                                message.Body = "Kolega " + po.Name + " " + po.Surname + ", vasi zaposleni NE RADE dobro posao. Slede penali!";
+                                message.To.Add(new MailAddress(po.Email));
+                                try
+                                {
+                                    smtpClient.Send(message);
+                                }
+                                catch (Exception exc)
+                                {
+                                    throw new FaultException(exc.Message);
+                                }
+
+                            }
+                        }
+                    }
                 }
             }
         }
