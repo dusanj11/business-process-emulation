3b0a79060b22b78a09206c9916f422dad68bb9de
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-01-15 09:25:25 +0100

    Novi testovi. Izmena kod logovanja korisnika, jer se nije na dobar način proveravalo da li je prošlo 6 meseci. Sada je provera dobra.

---
+++ Moduo1/HiringCompanyClientTest/Command/ShowCompaniesCommandTest.cs
@@ -0,0 +1,90 @@
+﻿using Client;
+using Client.Command;
+using Client.Model;
+using Client.ViewModel;
+using Client.ViewModelInterfaces;
+using HiringCompanyContract;
+using HiringCompanyData;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringCompanyClientTest.Command
+{
+    [TestFixture]
+    public class ShowCompaniesCommandTest
+    {
+        #region Declarations
+
+        private ShowCompaniesCommand showCompaniesCommandUnderTest;
+        private ObservableCollection<OutsourcingCompany> res = new ObservableCollection<OutsourcingCompany>();
+        #endregion Declarations
+
+        #region setup
+
+        [OneTimeSetUp]
+        public void SetupTest()
+        {
+
+            this.showCompaniesCommandUnderTest = new ShowCompaniesCommand();
+            this.showCompaniesCommandUnderTest.CanExecuteChanged += (object sender, EventArgs e) => { Console.WriteLine("CanExecuteChanged"); };
+
+            ClientDialogViewModel.Instance = Substitute.For<IClientDialogViewModel>();
+            ClientDialogViewModel.Instance.LogInUser().ReturnsForAnyArgs(new LogInUser());
+
+            ClientProxy.Instance = Substitute.For<IHiringCompany>();
+            ClientProxy.Instance.GetHcIdForUser("").ReturnsForAnyArgs(7);
+            ClientProxy.Instance.GetPartnershipOc(7).ReturnsForAnyArgs(new List<OutsourcingCompany>()
+            {
+                new OutsourcingCompany() {Name = "lol" }
+            });
+
+            ClientDialogViewModel.Instance.OcResources(res);
+            ClientDialogViewModel.Instance.ShowShowCompaniesView();
+
+
+        }
+
+        #endregion setup
+
+        #region Tests
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.DoesNotThrow(() => new ShowCompaniesCommand());
+        }
+
+        [Test]
+        public void CanExecuteTest()
+        {
+            Assert.DoesNotThrow(() => { showCompaniesCommandUnderTest.CanExecute(new Object()); });
+        }
+
+        [Test]
+        public void CanExecuteNullParametersTest()
+        {
+            Assert.DoesNotThrow(() => { showCompaniesCommandUnderTest.CanExecute(null); });
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+
+            Assert.DoesNotThrow(() => { showCompaniesCommandUnderTest.Execute(new object()); });
+        }
+
+        [Test]
+        public void ExecuteNullParametersTest()
+        {
+            Assert.DoesNotThrow(() => { showCompaniesCommandUnderTest.Execute(null); });
+        }
+
+        #endregion Tests
+    }
+}
--- Moduo1/HiringCompanyClientTest/Command/SignInCommandTest.cs
+++ Moduo1/HiringCompanyClientTest/Command/SignInCommandTest.cs
@@ -1,7 +1,9 @@
 ﻿using Client;
 using Client.Command;
 using Client.ViewModel;
+using Client.ViewModelInterfaces;
 using HiringCompanyContract;
+using HiringCompanyData;
 using HiringCompanyService.Access;
 using NSubstitute;
 using NUnit.Framework;
@@ -32,23 +34,56 @@ namespace HiringCompanyClientTest.Command
             this.signInCommandUnderTest = new SignInCommand();
             this.signInCommandUnderTest.CanExecuteChanged += (object sender, EventArgs e) => { Console.WriteLine("CanExecuteChanged"); };
 
-            //EmployeeDB.Instance = Substitute.For<IEmployeeDB>();
-
-            Client.View.AddEmployeeView view = Substitute.For<Client.View.AddEmployeeView>();
-
             ClientProxy.Instance = Substitute.For<IHiringCompany>();
-
+            ClientDialogViewModel.Instance = Substitute.For<IClientDialogViewModel>();
             ClientDialogViewModel.Instance.LogInUser().Returns(new Client.Model.LogInUser()
             {
                 Username = "naci",
                 Password = "naci"
             });
+            ClientDialogViewModel.Instance.ErrorMessage("");
+            ClientProxy.Instance.GetEmployee("dule", "dule").ReturnsForAnyArgs(new Employee()
+            {
+                Username = " dule", Password = "dule", Login = false, Position = "PO", PasswordUpadateDate = new DateTime(2014, 5, 5)
+            });
+            ClientProxy.Instance.EmployeeLogIn("dule").ReturnsForAnyArgs(true);
+
+
         }
 
         #endregion setup
 
 
         #region Tests
+        [Test]
+        public void ExecuteTestPO()
+        {
+            Assert.DoesNotThrow(() => { signInCommandUnderTest.Execute(new object()); });
+        }
+
+        [Test]
+        public void ExecuteTestHR()
+        {
+            ClientProxy.Instance.GetEmployee(null, null).ReturnsForAnyArgs(new Employee() { Position = "HR", PasswordUpadateDate = new DateTime(2014, 5, 5) });
+
+            Assert.DoesNotThrow(() => { signInCommandUnderTest.Execute(new object()); });
+        }
+
+        [Test]
+        public void ExecuteTestCEO()
+        {
+            ClientProxy.Instance.GetEmployee(null, null).ReturnsForAnyArgs(new Employee() { Position = "CEO", PasswordUpadateDate = new DateTime(2014, 5, 5) });
+
+            Assert.DoesNotThrow(() => { signInCommandUnderTest.Execute(new object()); });
+        }
+
+        [Test]
+        public void ExecuteTestSM()
+        {
+            ClientProxy.Instance.GetEmployee(null, null).ReturnsForAnyArgs(new Employee() { Position = "SM", PasswordUpadateDate = new DateTime(2014, 5, 5) });
+
+            Assert.DoesNotThrow(() => { signInCommandUnderTest.Execute(new object()); });
+        }
 
         [Test]
         public void ConstructorTest()
