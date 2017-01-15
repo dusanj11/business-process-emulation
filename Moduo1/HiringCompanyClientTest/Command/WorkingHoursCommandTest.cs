db65024a5a9e203b91fab6d44a392e2a9baa8e32
Dusan Jeftic <dusan.jeftic@outlook.com>
2017-01-15 02:47:20 +0100

    Dodatni testovi napravljeni.

---
+++ Moduo1/HiringCompanyClientTest/Command/WorkingHoursCommandTest.cs
@@ -0,0 +1,74 @@
+﻿using Client.Command;
+using Client.ViewModel;
+using Client.ViewModelInterfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringCompanyClientTest.Command
+{
+    [TestFixture]
+    public class WorkingHoursCommandTest
+    {
+        #region Declarations
+
+        private WorkingHoursCommand workingHoursCommandUnderTest;
+
+        #endregion Declarations
+
+        #region setup
+
+        [OneTimeSetUp]
+        public void SetupTest()
+        {
+            this.workingHoursCommandUnderTest = new WorkingHoursCommand();
+            this.workingHoursCommandUnderTest.CanExecuteChanged += (object sender, EventArgs e) => { Console.WriteLine("CanExecuteChanged"); };
+
+            ClientDialogViewModel.Instance = Substitute.For<IClientDialogViewModel>();
+            ClientDialogViewModel.Instance.ShowWorkingHoursView();
+
+
+        }
+
+        #endregion setup
+
+        #region Tests
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.DoesNotThrow(() => new AddEmployeeCommand());
+        }
+
+        [Test]
+        public void CanExecuteTest()
+        {
+            Assert.DoesNotThrow(() => { workingHoursCommandUnderTest.CanExecute(new Object()); });
+        }
+
+        [Test]
+        public void CanExecuteNullParametersTest()
+        {
+            Assert.DoesNotThrow(() => { workingHoursCommandUnderTest.CanExecute(null); });
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+
+            Assert.DoesNotThrow(() => { workingHoursCommandUnderTest.Execute(new object()); });
+        }
+
+        [Test]
+        public void ExecuteNullParametersTest()
+        {
+            Assert.DoesNotThrow(() => { workingHoursCommandUnderTest.Execute(null); });
+        }
+
+        #endregion Tests
+    }
+}
