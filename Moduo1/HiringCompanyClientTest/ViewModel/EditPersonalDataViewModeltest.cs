0cdc9903627a568b296f90264fe36dc01847e573
Dusan Jeftic <dusan.jeftic11@gmail.com>
2017-01-15 17:25:05 +0100

    Novi unit testovi.

---
+++ Moduo1/HiringCompanyClientTest/ViewModel/EditPersonalDataViewModeltest.cs
@@ -0,0 +1,68 @@
+﻿using Client.Command;
+using Client.ViewModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringCompanyClientTest.ViewModel
+{
+    [TestFixture]
+    public class EditPersonalDataViewModeltest
+    {
+        #region Declarations
+        private EditPersonalDataViewModel editPersonalDataViewModelUnderTest;
+        private EditPersonalDataCommand editPositionCommandUnderTest;
+        #endregion Declarations
+
+        #region setup
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            editPersonalDataViewModelUnderTest = new EditPersonalDataViewModel();
+            editPositionCommandUnderTest = new EditPersonalDataCommand();
+            string username = editPersonalDataViewModelUnderTest.Username;
+            string name = editPersonalDataViewModelUnderTest.Name;
+            string surname = editPersonalDataViewModelUnderTest.Surname;
+        }
+        #endregion setup
+
+        #region Tests
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.DoesNotThrow(() => new EditPersonalDataCommand());
+        }
+
+        [Test]
+        public void ConstructorTest2()
+        {
+            Assert.DoesNotThrow(() => new EditPersonalDataViewModel());
+        }
+
+        [Test]
+        public void ChangedCall()
+        {
+            Assert.DoesNotThrow(() => editPersonalDataViewModelUnderTest.OnPropertyChanged(null));
+        }
+
+        [Test]
+        public void Call1()
+        {
+            Assert.DoesNotThrow(() => EditPersonalDataViewModel.Instance.Name());
+        }
+        [Test]
+        public void Call2()
+        {
+            Assert.DoesNotThrow(() => EditPersonalDataViewModel.Instance.Surname());
+        }
+        [Test]
+        public void Call3()
+        {
+            Assert.DoesNotThrow(() => EditPersonalDataViewModel.Instance.Username());
+        }
+        #endregion Tests
+    }
+}
---
+++ Moduo1/HiringCompanyClientTest/ViewModel/EditPositionViewModelTest.cs
@@ -0,0 +1,64 @@
+﻿using Client.Command;
+using Client.ViewModel;
+using HiringCompanyData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringCompanyClientTest.ViewModel
+{
+    [TestFixture]
+    public class EditPositionViewModelTest
+    {
+        #region Declarations
+        private EditPositionViewModel editPositionViewModelUnderTest;
+        private EditPositionCommand editPositionCommandUnderTest;
+        #endregion Declarations
+
+        #region setup
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            editPositionViewModelUnderTest = new EditPositionViewModel();
+            editPositionCommandUnderTest = new EditPositionCommand();
+        }
+        #endregion setup
+
+        #region Tests
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.DoesNotThrow(() => new EditPositionCommand());
+        }
+
+        [Test]
+        public void ConstructorTest2()
+        {
+
+            Employee em = editPositionViewModelUnderTest.Employee;
+            string pos = editPositionViewModelUnderTest.Position;
+            Assert.DoesNotThrow(() => new EditPositionViewModel());
+        }
+
+        [Test]
+        public void ChangedCall()
+        {
+            Assert.DoesNotThrow(() => editPositionViewModelUnderTest.OnPropertyChanged(null));
+        }
+
+        [Test]
+        public void Call1()
+        {
+            Assert.DoesNotThrow(() => EditPositionViewModel.Instance.Employee());
+        }
+        [Test]
+        public void Call2()
+        {
+            Assert.DoesNotThrow(() => EditPositionViewModel.Instance.Position());
+        }
+        #endregion Tests
+    }
+}
